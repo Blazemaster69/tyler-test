fc76fb5214c8bde9b6f3107b5ca9444cb2e02f91
Blaze. Aka nikhil gulhane <89384291+Blazemaster69@users.noreply.github.com>
2021-12-08 19:44:17 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -21,11 +21,11 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // fetch result of https://waifu.pics/api/sfw/waifu from the API using axios
-        return void M.reply( await request.buffer(`https://i.pinimg.com/originals/d2/f0/0b/d2f00baf0b0d0535cfb902fcc16e7388.jpg`),
+        return void M.reply( await request.buffer(`https://i.pinimg.com/474x/b7/f8/af/b7f8af06a1f3f40aad8120a302a05c21--fight-club-rules.jpg`),
         MessageType.image,
                     undefined,
                     undefined,
-                    `_*----⚡[Rule for using the bot are below]⚡ ---*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use !support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *!tada/chat (your text)* both are different ai chat bots\n*--->* If you want to add Star Chan in your group the contact the owner by *!owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
+                    `_*----⚡[Rules for using the bot are below]⚡ ---*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use ?support to get the Official group link in your dm\n*--->*  If you want to chat with tyler you can use *?tada/chat (your text)* both are different ai chat bots\n*--->* If you want to add tyler sama in your group the contact the owner by *?owner/?mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
