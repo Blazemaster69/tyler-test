7e95cf591b1a789cbacf2e7128aee6dfe0e0c194
Blaze. Aka nikhil gulhane <89384291+Blazemaster69@users.noreply.github.com>
2021-12-08 15:39:40 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -1,34 +1,49 @@
 /** @format */
 
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "hi",
-			description: "Generally used to check if bot is Up",
-			category: "general",
-			usage: `${client.config.prefix}hi`,
-			baseXp: 10,
-		});
-	}
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'hi',
+            description: 'Say hi to the bot.',
+            category: 'general',
+            usage: `${client.config.prefix}hi`
+        })
+    }
 
-	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const chitoge =
-			"https://media.tenor.com/videos/8a6408675b1f806290aaac4b939c5a98/mp4";
-		return void this.client.sendMessage(
-			M.from,
-			{ url: chitoge },
-			MessageType.video,
-			{
-				quoted: M.WAMessage,
-				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
-			}
-		);
-	};
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/videos/tyler-hi.mp4'
+        ]
+        let tyler = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: tyler }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `⚡ hi lets get started ,the first rule of fight club is dont talk about fight club .. \n` }
+        )
+    }
 }
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
