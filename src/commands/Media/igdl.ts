78eef49901b082de66ef7aac840ffad520a585f3
Blaze. Aka nikhil gulhane <89384291+Blazemaster69@users.noreply.github.com>
2021-12-08 16:04:34 +0530

    Update igdl.ts

--- src/commands/Media/igdl.ts
+++ src/commands/Media/igdl.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
         while (true) {
             try {
                 M.reply(
-                    buffer || '🌟 An error occurred. Please try again later',
+                    buffer || '⚡ An error occurred. Please try again later',
                     MessageType.video,
                     undefined,
                     undefined,
@@ -40,7 +40,7 @@ export default class Command extends BaseCommand {
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
                     // console.log('Failed')
-                    M.reply(`🌟An error occurred. Please try again later.`)
+                    M.reply(`⚡ An error occurred. Please try again later.`)
                 })
                 break
             } catch (e) {
