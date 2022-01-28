d16f09cead5781991f43b89ac8256253f3b71ec0
Blaze. Aka nikhil gulhane <89384291+Blazemaster69@users.noreply.github.com>
2022-01-29 02:04:24 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
         text += `\n© tyler durden by blaze sama 
- follow 🆔 blazesama_69 on insta and dm to add the bot 
+ follow 🆔 blazesama_69 on insta to add the bot 
  link : https://www.instagram.com/blazesama_69`
         return void M.reply(text)
     }
