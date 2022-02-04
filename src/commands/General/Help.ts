72a4ec3edec29d9830420a903792b6bd9e6ff052
Blaze. Aka nikhil gulhane <89384291+Blazemaster69@users.noreply.github.com>
2022-02-04 22:17:48 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -56,6 +56,7 @@ export default class Command extends BaseCommand {
             caption: `${text}
 ┌────────────┈火
 │  ✘ 𝕋𝕪𝕝𝕖𝕣-durden © by blaze sama
+│     FORKED FROM © CHITOGE VOID ANIME BOTTO
 |     follow 🆔 blazesama_69 on 
 |     insta and dm https://wa.me/+917721059975 to add the bot
 |     link : https://www.instagram.com/blazesama_69
